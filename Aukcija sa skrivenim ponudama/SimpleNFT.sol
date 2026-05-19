7bf59ba15d2bd760997c24868a7397a87d869cd8
sandramilosevic <smilosevic892@gmail.com>
2026-05-19 15:03:43 +0200

    simple nft

---
+++ Aukcija sa skrivenim ponudama/SimpleNFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; // biblioteka za generisanje NFT tokena
+
+contract SimpleNFT is ERC721URIStorage {
+    uint256 public tokenCounter; // brojac za generisane tokene - id
+
+    constructor() ERC721('AuctionNFT', 'ANFT') {
+        tokenCounter = 0; // pocetna vrednost brojaca
+    }
+
+    function mint(address owner, string memory tokenURI) public returns (uint256) {
+        uint256 id = tokenCounter;
+        tokenCounter ++;
+        _mint(owner, id); // kreiranje tokena
+        _setTokenURI(id, tokenURI); // postavljanje URI-a tokena
+        return id;
+    }
+}
